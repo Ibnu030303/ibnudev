5d926d527db046991545a717059bd4ea4983e4a6
Ibnu030303 <ibunu.nurdiyansa28@gmail.com>
2024-03-04 13:50:14 +0700

    ibnudev

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,9 @@
+const toggleButton = document.getElementById("toggleButton");
+
+toggleButton.addEventListener("click", function () {
+  document.body.classList.toggle("dark-mode");
+});
+
 const tooltipTriggerList = document.querySelectorAll(
   '[data-bs-toggle="tooltip"]'
 );
